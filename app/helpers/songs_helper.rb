d6e33dbd88249b46fd2223ad80fbbc02ceabe445
+ <+>
2021-07-03 19:58:23 -0700

    commit tree b9f757118c3747e57431f94af7be1b3dbae81bfb

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,11 @@
 module SongsHelper
 
+    def link_to_new_song
+        if params[:artist_id]
+            link_to "Add New Song", new_artist_song_path(Artist.find_by_id(params[:artist_id]))
+        else
+            link_to "Add New Song", new_song_path
+        end
+    end
+
 end
